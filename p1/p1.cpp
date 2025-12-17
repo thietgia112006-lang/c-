fbc8040720b732c8e2b746935ada61757e515b0c
gia truong <thietgia112006@gmail.com>
2025-12-17 19:10:59 +0700

    c++

--- p1/p1.cpp
+++ p1/p1.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 int main()
 {
-    float a, b, c;
-    cout << "nhap a, b, c: " << endl;
+    int a, b, c;
+    cout << "nhap a, b, c: ";
     cin >> a >> b >> c;
     if (a == 0) {
         if (b == 0) {
@@ -22,23 +22,23 @@ int main()
         else
             cout << "nghiem phuong trinh la: " << - c * 1.0 / a << endl;
     }
-    else
-        float d = b * b - 4 * a * c;
-            if ( d == 0 ) 
-                {
-                cout << "phuong trinh co nghiem kep" << -b * 1, 0 / (2 * a);
+    else {
+        int d = b * b - 4 * a * c;
+        if (d == 0) {
+            cout << "phuong trinh co nghiem kep: " << -b * 1, 0 / (2 * a);
 
-                }
-                else {
-                    if (d < 0) {
-                        cout << "phuong trinh vo nghiem" << endl;
+        }
+        else {
+            if (d < 0) {
+                cout << "phuong trinh vo nghiem" << endl;
 
-                    }
-                    else
-                        cout << "nghiem x1 : " << (-b - sqrt(d)) / (2 * a) << endl;
-                        cout << "nghiem x2 : " << (-b + sqrt(d)) / (2 * a) << endl;
-            
-            
             }
+            else
+                cout << "nghiem x1 : " << (-b - sqrt(d)) / (2 * a) << endl;
+            cout << "nghiem x2 : " << (-b + sqrt(d)) / (2 * a) << endl;
+
+
+        }
+    }
 }
 
